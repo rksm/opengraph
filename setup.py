fdcf60c9bd6067aa82a66779a630b1dc315921fe
Thomas Grainger <tagrain@gmail.com>
2012-08-01 16:30:06 +0100

    bump version switch to distutils

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-from setuptools import setup, find_packages
-import sys, os
+from os.path import join, dirname
+from distutils.core import setup
 
-version = '0.9'
+try:
+    f = open(join(dirname(__file__), 'README.rst'))
+    long_description = f.read().strip()
+    f.close()
+except IOError:
+    long_description = ""
+
+
+version = '0.9.1'
 
 setup(
     name='ogp',
     version=version,
     description="A module to parse the Open Graph Protocol",
-    long_description=open("README.rst").read() + "\n",
+    long_description=long_description + "\n",
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -22,6 +30,5 @@ setup(
     license='MIT',
     packages=["ogp",],
     install_requires=['BeautifulSoup'],
-    data_files=[('', ['README.rst',])]
 )
     
